f29809be5c072eeda185f01ed5c81eed11f9e5b1
Nezar Alazzabi <Nalazzabi@hotmail.com>
2018-02-17 05:12:00 -0800

    Add files via upload

---
+++ Using JSON API with VB.NET/Home.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BunifuTileButton2 = New ns1.BunifuTileButton()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.mygridview = New ns1.BunifuCustomDataGrid()
+        Me.Column1 = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.mygridview, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.BunifuTileButton2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(157, 586)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.Label4.Font = New System.Drawing.Font("Eras Medium ITC", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(9, 150)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(137, 151)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Simple Application that show's how to work with " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Json Api's in Desktop Applicati" &
+    "on ..."
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.Label3.Font = New System.Drawing.Font("Eras Medium ITC", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Lime
+        Me.Label3.Location = New System.Drawing.Point(24, 539)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 38)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Developed By" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Nezar Alazzabi"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BunifuTileButton2
+        '
+        Me.BunifuTileButton2.BackColor = System.Drawing.Color.Transparent
+        Me.BunifuTileButton2.color = System.Drawing.Color.Transparent
+        Me.BunifuTileButton2.colorActive = System.Drawing.Color.MediumSeaGreen
+        Me.BunifuTileButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BunifuTileButton2.Font = New System.Drawing.Font("Century Gothic", 15.75!)
+        Me.BunifuTileButton2.ForeColor = System.Drawing.Color.White
+        Me.BunifuTileButton2.Image = Global.Using_JSON_API_with_VB.NET.My.Resources.Resources.Home_64px
+        Me.BunifuTileButton2.ImagePosition = 10
+        Me.BunifuTileButton2.ImageZoom = 50
+        Me.BunifuTileButton2.LabelPosition = 41
+        Me.BunifuTileButton2.LabelText = "Exit"
+        Me.BunifuTileButton2.Location = New System.Drawing.Point(15, 15)
+        Me.BunifuTileButton2.Margin = New System.Windows.Forms.Padding(6)
+        Me.BunifuTileButton2.Name = "BunifuTileButton2"
+        Me.BunifuTileButton2.Size = New System.Drawing.Size(128, 129)
+        Me.BunifuTileButton2.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(157, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1096, 54)
+        Me.Panel2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Elephant", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(470, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(248, 35)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CURRENCY EX"
+        '
+        'mygridview
+        '
+        Me.mygridview.AllowUserToAddRows = False
+        Me.mygridview.AllowUserToDeleteRows = False
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.mygridview.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.mygridview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.mygridview.BackgroundColor = System.Drawing.Color.White
+        Me.mygridview.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.mygridview.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer))
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.Color.Lime
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.mygridview.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.mygridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.mygridview.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle3.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Tahoma", 8.0!)
+        DataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.mygridview.DefaultCellStyle = DataGridViewCellStyle3
+        Me.mygridview.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.mygridview.DoubleBuffered = True
+        Me.mygridview.EnableHeadersVisualStyles = False
+        Me.mygridview.GridColor = System.Drawing.Color.White
+        Me.mygridview.HeaderBgColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer))
+        Me.mygridview.HeaderForeColor = System.Drawing.Color.Lime
+        Me.mygridview.Location = New System.Drawing.Point(157, 54)
+        Me.mygridview.Name = "mygridview"
+        Me.mygridview.ReadOnly = True
+        Me.mygridview.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        DataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle4.Font = New System.Drawing.Font("Century Gothic", 11.25!)
+        Me.mygridview.RowsDefaultCellStyle = DataGridViewCellStyle4
+        Me.mygridview.Size = New System.Drawing.Size(1096, 532)
+        Me.mygridview.TabIndex = 2
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Flag"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        Me.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Currency"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Price"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Highest Price / Year"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Exchange Rate"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Update Date"
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        '
+        'Home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1253, 586)
+        Me.Controls.Add(Me.mygridview)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Home"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.mygridview, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents BunifuTileButton2 As ns1.BunifuTileButton
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents mygridview As ns1.BunifuCustomDataGrid
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Column1 As DataGridViewImageColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+End Class
---
+++ Using JSON API with VB.NET/Home.vb
@@ -0,0 +1,72 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Bunifu
+Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json
+Imports System.Linq
+
+Public Class Home
+
+    Private Sub printJSON(jsonStr As String, ByVal currency As String, ByVal icon As Image)
+
+        ' Json.NET deserializer gives you a list of .NET objects
+        Dim Eassel = JsonConvert.DeserializeObject(Of List(Of JSON_data))(jsonStr)
+
+
+
+        ' Set up a LINQ statement to filter the monarchs list
+        Dim EasselList = From monarch In Eassel Where monarch.n.Contains(currency) Select monarch
+
+        ' Print the results of our LINQ query
+        For Each EasselX In EasselList
+            mygridview.Rows.Add(icon, EasselX.n, EasselX.v, EasselX.k, EasselX.cp, EasselX.d.Substring(0, EasselX.d.Length - 14))
+        Next
+
+        'Loop Through Gridview to change the background color of the cell in case currency value were down 
+        For i As Integer = 0 To mygridview.Rows.Count - 1
+            If mygridview.Rows(i).Cells(4).Value.ToString.Contains("-") Then
+                mygridview.Rows(i).Cells(4).Style.BackColor = Color.Red
+            Else
+                mygridview.Rows(i).Cells(4).Style.BackColor = Color.White
+            End If
+        Next
+    End Sub
+    Public Class JSON_data
+        'Defining the variables that holds data from Json File
+        Public n As String
+        Public k As String
+        Public v As String
+        Public cp As String
+        Public d As String
+    End Class
+
+    Private Sub FinalFunction()
+        Dim errorMsg As String = Nothing
+        Try
+            'Define the json url
+            Dim jsonURL As String = "http://essale.herokuapp.com/api/tick"
+            Dim reader As StreamReader
+            ' make an HTTP request to our json url
+            Dim request As HttpWebRequest = CType(WebRequest.Create(jsonURL), HttpWebRequest)
+            Dim response As HttpWebResponse = CType(request.GetResponse(), HttpWebResponse)
+            'Read Data from Json File
+            reader = New StreamReader(response.GetResponseStream())
+            Dim jsonStr As String = reader.ReadToEnd()
+            'Sending the currency name to brings back the data from json file
+            printJSON(jsonStr, "USD", My.Resources.USA_32px)
+            printJSON(jsonStr, "EUR", My.Resources.Flag_Of_Europe_32px)
+            printJSON(jsonStr, "GBP", My.Resources.Great_Britain_32px)
+            printJSON(jsonStr, "TUN", My.Resources.Tunisia_32px)
+            printJSON(jsonStr, "TUR", My.Resources.Turkey_32px)
+            printJSON(jsonStr, "EGP", My.Resources.Egypt_32px)
+            printJSON(jsonStr, "CJM", My.Resources.Transaction_32px)
+        Catch ex As WebException
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Home_Load(sender As Object, e As EventArgs) Handles Me.Load
+        FinalFunction()
+    End Sub
+End Class
